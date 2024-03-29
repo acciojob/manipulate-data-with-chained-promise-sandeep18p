a0ef6920a246e7c046794a102c53d9584cb28a95
Sandeep Pansari <86551476+sandeep18p@users.noreply.github.com>
2024-03-29 16:52:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-//your JS code here. If required.
+function manipulateData(inputArray) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(inputArray);
+        }, 3000);
+    });
+}
+
+const outputElement = document.getElementById('output');
+
+const inputArray = [1, 2, 3, 4];
+
+manipulateData(inputArray)
+    .then(filteredArray => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const evenNumbers = filteredArray.filter(num => num % 2 === 0);
+                resolve(evenNumbers);
+            }, 1000);
+        });
+    })
+    .then(evenNumbers => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const multipliedArray = evenNumbers.map(num => num * 2);
+                resolve(multipliedArray);
+            }, 2000);
+        });
+    })
+    .then(resultArray => {
+        outputElement.innerText = resultArray.join(', ');
+    })
+    .catch(error => {
+        outputElement.innerText = `Error: ${error.message}`;
+    });
